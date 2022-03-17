fd0f5812ea45a2da940845f975d2cd7a3462842c
SmashingPrograms <smashingprograms@gmail.com>
2022-03-17 16:14:21 -0400

    Replaces string substrings with button elements

--- frontend/static/src/components/game.js
+++ frontend/static/src/components/game.js
@@ -1,5 +1,6 @@
 import Cookies from 'js-cookie';
 import { useState } from 'react';
+import reactStringReplace from 'react-string-replace';
 
 function Game(props) {
   const [data, setData] = useState(null)
@@ -29,25 +30,54 @@ function Game(props) {
     };
   };
 
+  let promptPassage;
+  let blanks = 6;
+
+  if (data) {
+    promptPassage = data?.prompt_passage
+    console.log("Got here")
+    promptPassage = reactStringReplace(promptPassage, /_____\(([0-9])\)/g, (match, i) => (
+      <button key={match}>{match}</button>
+    ));
+    console.log("Got here too")
+    // for (let i = 1; i <= blanks; i++) {
+    //   const substring = new RegExp(``);
+    // promptPassage = promptPassage.replace(/_____\(([0-9])\)/g, <span key='$1'>_____</span>)
+    // }
+    // const blankInstances = .matchAll(substring);
+    // const blanks = [];
+    // for (const instance of blankInstances) {
+    //   blanks.push(instance[0]);
+    // };
+  }
+
+  // console.log(<span key='1'>_____</span>)
   const gamePrompt = (
+    <>
+      {promptPassage ? promptPassage : ""}
+    </>
+  )
+
+  const game = (
     <>
       <p>{data?.book_title} ({data?.book_year}) by {data?.book_author}</p>
       <p>Genre: {data?.book_genre}</p>
       <div>
-        {data?.prompt_passage}
+        {gamePrompt}
       </div>
       <div>
         {data?.word_choices.map((word, index) => (
           <button key={index}>{word}</button>
         ))}
       </div>
+      <button>Get a hint</button>
     </>
   )
 
   return (
     <>
       <button onClick={handleSubmit}>Next round</button>
-      <div>{data ? gamePrompt : "Click the button above!"}</div>
+      <div>{data ? game : "Click the button above!"}</div>
     </>
   )
 };
